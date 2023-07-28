71803272a0edc139fbcf6d77db2085718590a177
EdogawaConan2023 <developervn01072023@gmail.com>
2023-07-29 00:50:32 +0700

    update

---
+++ script.js
@@ -0,0 +1,123 @@
+// Values and Variables
+
+// let country = 'VietNam';
+// let continent = 'Asia';
+// let population = 100;
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// Data Types
+
+// let a = true
+// console.log (a);
+// a = 'yes'
+// console.log (a);
+
+// let b
+// console.log (b)
+// console.log (typeof b);
+// b = 1992
+// console.log (b);
+// console.log (typeof b)
+
+// let, const and var
+
+// let age = 30
+// age = 31
+
+// const brirthYear = 1992
+// brirthYear = 1990
+
+// console.log (age);
+// console.log (brirthYear);
+
+// class = "12A1"
+// console.log (class) ;
+
+
+// Basic Operators
+// // Toan tu so
+// const now = 2023
+// const ageQuynh = now - 1992;
+// const ageNhi = now - 2021;
+// console.log (ageNhi , ageQuynh);
+// console.log (ageNhi * 2 , ageQuynh / 2 , 2 ** 3);
+
+// const firstName = 'Do'
+// const lastName = 'Quynh'
+// console.log (firstName + ' ' + lastName);
+
+// // Toan tu gan
+
+// let x = 10
+// x += 10  // x = x + 10
+// x -= 10 // x = x - 10
+// x *= 10 // x = x * 10 
+// x++ 
+// x--
+// --x
+// console.log (x);
+
+// Toan tu so sanh
+
+// const now = 2023
+// const ageQuynh = now - 1992;
+// const ageNhi = now - 2021;
+// console.log (ageQuynh > ageNhi);
+
+// let number1 = parseInt(prompt("Nhập số thứ nhất:"));
+// let number2 = parseInt(prompt("Nhập số thứ hai:"));
+
+// let sum = number1 + number2;
+// let difference = number1 - number2;
+
+// console.log("Tổng của hai số là: " + sum);
+// console.log("Hiệu của hai số là: " + difference);
+
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+// const BMIMark = massMark / (heightMark * heightMark);
+// const BMIJohhn = massJohn / (heightJohn * heightJohn);
+// console.log (BMIJohhn);
+// console.log (BMIMark);
+// console.log (BMIMark > BMIJohhn);
+// console.log ( BMIJohhn ,BMIMark, BMIMark > BMIJohhn);
+
+// String
+
+// const firstName = 'Quynh';
+// const job = 'developer';
+// const brirthYear = 1992
+// const year = 2023;
+
+// const Quynh = 'Toi la' + ' ' + firstName + ' ' + 'Toi la' + ' ' +  1 + ' ' + job + ' ' + 'bat dau tu nam' + ' ' + (year - brirthYear);
+// console.log (Quynh);
+// const newQuynh = `toi la ${firstName}, Toi la 1 ${job}, bat dau tu nam 31 ; `
+// console.log (newQuynh)
+
+// console.log ('toi muon thanh thao JS \n\
+// ok \n\
+// ');
+
+// if/else 😂
+const age = 19;
+const isOldEnough = age >= 18;
+if (isOldEnough) {
+    console.log ('Duoc phep lai xe ☺️')
+} else  {
+    console.log ('Khong duoc phep lai xe 😢')
+}
+
+
+
+
+
+
+
+
+
+
